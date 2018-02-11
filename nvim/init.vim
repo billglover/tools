649dbcae85a1dd2811ea3241e6af88c7254ab9e1
Bill Glover <bill@billglover.co.uk>
2018-02-11 00:17:35 +0000

    Add basic nvim configuration

---
+++ nvim/init.vim
@@ -0,0 +1,7 @@
+let g:python_host_prog  = '/usr/bin/python'
+let g:python3_host_prog = '/usr/bin/python3'
+
+call plug#begin()
+Plug 'fatih/vim-go'
+Plug 'scrooloose/nerdtree'
+call plug#end()
---
+++ nvim/setup.sh
@@ -0,0 +1,7 @@
+pip2 install --user --upgrade neovim
+pip3 install --user --upgrade neovim
+gem install neovim
+npm install -g neovim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
